88fbeec0e9263df247f089bb813b750fe71dc312
Jun Wakaisami <e1c16105@st.oit.ac.jp>
2020-02-02 17:44:34 +0900

    バッヂの種類の追加

--- db/fixtures/17_condition_badge.rb
+++ db/fixtures/17_condition_badge.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 ConditionBadge.seed_once(:id,
-    { id: 1, name:'いいね10', image:"いいね銅" },
-    { id: 2, name:'いいね50', image:"いいね銀" },
-    { id: 3, name:'いいね100', image:"いいね金" },
-    { id: 4, name:'コメント5', image:"コメント銅" },
-    { id: 5, name:'コメント15', image:"コメント銀" },
-    { id: 6, name:'コメント50', image:"コメント金" },
-    { id: 7, name:'ログイン3', image:"ログイン銅" },
-    { id: 8, name:'ログイン30', image:"ログイン銀" },
-    { id: 9, name:'ログイン90', image:"ログイン金" }
+    { id: 1, name:'LIKE10', image:"like_bronze" },
+    { id: 2, name:'LIKE30', image:"like_silver" },
+    { id: 3, name:'LIKE100', image:"like_gold" },
+    { id: 4, name:'COMMENT5', image:"comment_bronze" },
+    { id: 5, name:'COMMENT15', image:"comment_silver" },
+    { id: 6, name:'COMMENT50', image:"comment_gold" },
+    { id: 7, name:'LOGIN3', image:"login_bronze" },
+    { id: 8, name:'LOGIN10', image:"login_silver" },
+    { id: 9, name:'LOGIN30', image:"login_gold" }
 )
