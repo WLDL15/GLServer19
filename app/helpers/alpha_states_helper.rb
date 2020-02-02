045ca8aa97a71b024739a2b198d35d0d2ede88dc
yoshikawa naoki <millennium_myonn@yahoo.co.jp>
2020-02-02 17:43:07 +0900

    ツールチップを使用した辞書機能作成

--- app/helpers/alpha_states_helper.rb
+++ app/helpers/alpha_states_helper.rb
@@ -1,2 +1,13 @@
 module AlphaStatesHelper
+    def return_tooltip(in_text)
+        in_text.split("/").each do |text|
+            if Dictionary.find_by(name: text) && text == Dictionary.find_by(name: text).name
+                return_text = ""
+                return_text << "<span style=\"border-bottom: solid 1px black; data-toggle=\"tooltip\" data-html=\"true\" title=" << Dictionary.find_by(name: text).description << ">" << text << "</span>" 
+                in_text.gsub!("/" << text << "/", return_text)
+            end            
+        end
+        return in_text
+    end
+
 end
