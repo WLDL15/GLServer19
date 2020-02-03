e1f8f9b5b152378151f29dcad6e831b7881bfc6f
yoshikawa naoki <millennium_myonn@yahoo.co.jp>
2020-02-03 17:34:33 +0900

    custon,assets,gitignore変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( print.css 
-    application.scss
-)
+Rails.application.config.assets.precompile += %w( theme.scss custom.scss )
