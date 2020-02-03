064cbcce75e46c6b2d042ade0c95e4e6fdf368d3
yoshikawa naoki <millennium_myonn@yahoo.co.jp>
2020-02-03 18:35:48 +0900

    migration変更

--- db/migrate/20171214103130_change_role_to_scrum_members.rb
+++ db/migrate/20171214103130_change_role_to_scrum_members.rb
@@ -1,5 +1,5 @@
 class ChangeRoleToScrumMembers < ActiveRecord::Migration[5.1]
   def change
-  	change_column :scrum_members, :role, :integer, 'numeric USING CAST(target_column AS numeric)'
+  	change_column :scrum_members, :role, 'integer USING CAST(role AS integer)'
   end
 end
