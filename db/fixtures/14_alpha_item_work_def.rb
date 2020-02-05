71ef22a580a140fbaa8e31517010c8c5910f624c
yuu0797 <e1c16052@st.oit.ac.jp>
2020-02-05 20:03:37 +0900

    チェックリストの変更　仕事。、チーム

--- db/fixtures/14_alpha_item_work_def.rb
+++ db/fixtures/14_alpha_item_work_def.rb
@@ -2,50 +2,57 @@
 
 # 仕事1/5 65
 AlphaItemDef.seed_once(:id,
-    { id: 65, dseq:'1', ddescription: '全員がプロジェクトのビジョンを共有するためのイベントを開催している', alpha_state_def_id: 11, item_level:2, item_point:3 },
-    { id: 66, dseq:'2', ddescription: '/プロダクトバックログ/の優先順位の基準が定義されている', alpha_state_def_id: 11, item_level:2, item_point:4 },
-    { id: 67, dseq:'3', ddescription: 'プロジェクトのリスク(技術、人的リソース、機関など)が識別されている', alpha_state_def_id: 11, item_level:2, item_point:4 },
-    { id: 68, dseq:'4', ddescription: '開発環境と開発ツールを決定している', alpha_state_def_id: 11, item_level:2, item_point:4 }
+    { id: 81, dseq:'1', ddescription: '全員がプロジェクトのビジョンを共有するためのイベントを開催している', alpha_state_def_id: 11, item_level:2, item_point:3 },
+    { id: 82, dseq:'2', ddescription: '開発環境と開発ツールを決定している', alpha_state_def_id: 11, item_level:1, item_point:4 },
+    { id: 83, dseq:'3', ddescription: 'スクラム開発法のイベントの目的を理解している', alpha_state_def_id: 11, item_level:2, item_point:4 },
+    { id: 84, dseq:'4', ddescription: 'スクラム開発法の成果物の目的を理解している', alpha_state_def_id: 11, item_level:2, item_point:4 }
 )
 
 # 仕事2/5 69
 AlphaItemDef.seed_once(:id,
-    { id: 69, dseq:'1', ddescription: '開発ツールを利用して/アーキテクチャ/のサンプルアプリが作成でき、開発環境において実行できることを確認している', alpha_state_def_id: 12, item_level:2, item_point:3 },
-    { id: 70, dseq:'2', ddescription: '開発環境と開発ツールをインストールする方法が明確になっている', alpha_state_def_id: 12, item_level:2, item_point:5 },
-    { id: 71, dseq:'3', ddescription: 'チーム全員が、開発環境と開発ツールをインストールする方法を理解している', alpha_state_def_id: 12, item_level:2, item_point:5 },
-    { id: 72, dseq:'4', ddescription: 'チーム全員が開発環境と開発ツールを利用できる状態にある', alpha_state_def_id: 12, item_level:2, item_point:4 },
-    { id: 73, dseq:'5', ddescription: '用する/アプリケーションフレームワーク/やライブラリの学習リソース（ウェブサイトや書籍など）のリストを作成している', alpha_state_def_id: 12, item_level:2, item_point:4 }
+    { id: 85, dseq:'1', ddescription: '発環境と開発ツールをインストールする方法が明確になっている', alpha_state_def_id: 12, item_level:1, item_point:3 },
+    { id: 86, dseq:'2', ddescription: '開発ツールを利用して、サンプルアプリを作成でき、開発環境において実行できることを確認している', alpha_state_def_id: 12, item_level:1, item_point:3 },
+    { id: 87, dseq:'3', ddescription: '開発チームは、開発環境と開発ツールを利用できる状態にある', alpha_state_def_id: 12, item_level:1, item_point:5 },
+    { id: 88, dseq:'4', ddescription: 'スクラム開発法のどのイベントを実施するかを決定している', alpha_state_def_id: 12, item_level:2, item_point:4 },
+    { id: 89, dseq:'5', ddescription: '/プロダクトバックログ/と/スプリントバックログ/に記述すべき項目を決定している', alpha_state_def_id: 12, item_level:2, item_point:4 }
 )
 
 # 仕事3/5 74
 AlphaItemDef.seed_once(:id,
-    { id: 74, dseq:'1', ddescription: 'メンバは自分の意志でタスクを選択し、作業を進めている', alpha_state_def_id: 13, item_level:3, item_point:3 },
-    { id: 75, dseq:'2', ddescription: 'チームはタスクのDoneの条件を定義し、それを守ることに同意している', alpha_state_def_id: 13, item_level:2, item_point:3 },
-    { id: 76, dseq:'3', ddescription: 'メンバはタスクのDoneの定義に従ってタスクの進捗状況を更新している', alpha_state_def_id: 13, item_level:3, item_point:3 },
-    { id: 77, dseq:'4', ddescription: 'メンバはタスクの進捗を適切に更新しており、全員がそれを容易に閲覧できる', alpha_state_def_id: 13, item_level:3, item_point:5 },
-    { id: 78, dseq:'5', ddescription: 'ソースが共有されており、定期的に最新版に更新されている', alpha_state_def_id: 13, item_level:3, item_point:5 },
-    { id: 79, dseq:'6', ddescription: '共有されたソースを使ってシステムを一定期間ごとに運用環境でビルドしている', alpha_state_def_id: 13, item_level:3, item_point:3 },
-    { id: 80, dseq:'7', ddescription: 'バグ情報が共有されており、バグの対策状況が追跡できている', alpha_state_def_id: 13, item_level:3, item_point:3 }
+    { id: 90, dseq:'1', ddescription: 'メンバは自分の意志でタスクを選択し、作業を進めている', alpha_state_def_id: 13, item_level:2, item_point:5 },
+    { id: 91, dseq:'2', ddescription: '開発環境と開発ツールを利用して開発している', alpha_state_def_id: 13, item_level:1, item_point:3 },
+    { id: 92, dseq:'3', ddescription: 'タスクのDoneの条件を定義し、それを守ることに同意している', alpha_state_def_id: 13, item_level:2, item_point:3 }
+    { id: 93, dseq:'4', ddescription: 'メンバはタスクのDoneの定義に従ってタスクの進捗状況を更新している', alpha_state_def_id: 13, item_level:3, item_point:3 },
+    { id: 94, dseq:'5', ddescription: 'メンバはタスクの進捗を適切に更新しており、全員がそれを容易に閲覧できる', alpha_state_def_id: 13, item_level:3, item_point:5 },
+    { id: 95, dseq:'6', ddescription: 'ソースが共有されており、定期的に最新版に更新されている', alpha_state_def_id: 13, item_level:3, item_point:5 },
+    { id: 96, dseq:'7', ddescription: '共有されたソースを使ってシステムを一定期間ごとに運用環境でビルドしている', alpha_state_def_id: 13, item_level:3, item_point:3 },
+    { id: 97, dseq:'8', ddescription: 'バグ情報が共有されており、バグの対策状況が追跡できている', alpha_state_def_id: 13, item_level:3, item_point:3 }
+
 )
 
 # 仕事4/5 81
 AlphaItemDef.seed_once(:id,
-    { id: 81, dseq:'1', ddescription: 'タスクの終了時に、完了日、作業時間などを記録している', alpha_state_def_id: 14, item_level:3, item_point:3 },
-    { id: 82, dseq:'2', ddescription: 'チームの/ベロシティ/を計測している', alpha_state_def_id: 14, item_level:2, item_point:4 },
-    { id: 83, dseq:'3', ddescription: 'タスクの進捗を可視化するためにスプリントの残作業時間をグラフ化している', alpha_state_def_id: 14, item_level:4, item_point:3 },
-    { id: 84, dseq:'4', ddescription: 'タスクのDoneの定義が守られているかを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 },
-    { id: 85, dseq:'5', ddescription: '共有されたソースが定期的に更新されているかを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 },
-    { id: 86, dseq:'6', ddescription: '共有されたソースを使ってシステムを一定期間ごとにビルドできているかを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 },
-    { id: 87, dseq:'7', ddescription: 'バグの対策状況が正しく追跡されていることを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 }
+ /   { id: 98, dseq:'1', ddescription: '/スプリント/でリリースする/インクリメント/のスコープと優先順位を理解している', alpha_state_def_id: 14, item_level:1, item_point:4 },
+ /   { id: 99, dseq:'2', ddescription: '/インクリメント/をリリースするために必要な作業が明確になっている', alpha_state_def_id: 14, item_level:1, item_point:5 },
+ /   { id: 100, dseq:'3', ddescription: 'マスタは、スプリントの終了時に/インクリメント/が完成するように、チームを支援している', alpha_state_def_id: 14, item_level:1, item_point:5 },
+    { id: 101, dseq:'4', ddescription: 'タスクのDoneの定義が守られているかを検査している', alpha_state_def_id: 14, item_level:2, item_point:3 },
+    { id: 102, dseq:'5', ddescription: 'タスクの終了時に、完了日、作業時間などを記録している', alpha_state_def_id: 14, item_level:3, item_point:3 },
+    { id: 103, dseq:'6', ddescription: 'チームの/ベロシティ/を計測している', alpha_state_def_id: 14, item_level:2, item_point:4 },
+    { id: 104, dseq:'7', ddescription: 'タスクの進捗を可視化するためにスプリントの残作業時間をグラフ化している', alpha_state_def_id: 14, item_level:4, item_point:3 },
+    { id: 105, dseq:'8', ddescription: '共有されたソースが定期的に更新されているかを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 },
+    { id: 106, dseq:'9', ddescription: '共有されたソースを使ってシステムを一定期間ごとにビルドできているかを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 },
+    { id: 107, dseq:'10', ddescription: 'バグの対策状況が正しく追跡されていることを検査している', alpha_state_def_id: 14, item_level:4, item_point:3 }
 )
 
 # 仕事5/5 88
 AlphaItemDef.seed_once(:id,
-    { id: 88, dseq:'1', ddescription: 'ソフトウェア製品の外部品質の６つの特性を理解している', alpha_state_def_id: 15, item_level:2, item_point:4 },
-    { id: 89, dseq:'2', ddescription: 'ソフトウェアテストのプロセスと技法を理解している', alpha_state_def_id: 15, item_level:5, item_point:4 },
-    { id: 90, dseq:'3', ddescription: 'ソースがレビューされ、担当者以外の人が理解できている', alpha_state_def_id: 15, item_level:5, item_point:4 },
-    { id: 91, dseq:'4', ddescription: '/オーナ/はシステムの機能と品質がリリース可能なレベルであるかを合意している', alpha_state_def_id: 15, item_level:4, item_point:4 },
-    { id: 92, dseq:'5', ddescription: 'チームはリリースのために不足している機能や品質を識別している', alpha_state_def_id: 15, item_level:5, item_point:4 },
-    { id: 93, dseq:'6', ddescription: 'チームはリリースのために追加作業が必要かを検討している', alpha_state_def_id: 15, item_level:5, item_point:4 },
-    { id: 94, dseq:'7', ddescription: 'チームはリリースに必要な追加作業を実施している', alpha_state_def_id: 15, item_level:5, item_point:5 }
+    { id: 108, dseq:'1', ddescription: '開発作業で発生した問題点と解決法をまとめている', alpha_state_def_id: 15, item_level:1, item_point:6 },
+    { id: 109, dseq:'2', ddescription: 'スクラム開発法を実施できているかを議論している', alpha_state_def_id: 15, item_level:2, item_point:4 },
+    { id: 110, dseq:'3', ddescription: 'ソフトウェアテストのプロセスと技法を理解している', alpha_state_def_id: 15, item_level:5, item_point:4 },
+    { id: 111, dseq:'4', ddescription: 'ソースがレビューされ、担当者以外の人が理解できている', alpha_state_def_id: 15, item_level:5, item_point:4 },
+    { id: 112, dseq:'5', ddescription: '/オーナ/はシステムの機能と品質がリリース可能なレベルであるかを合意している', alpha_state_def_id: 15, item_level:4, item_point:4 },
+    { id: 113, dseq:'6', ddescription: 'チームはリリースのために不足している機能や品質を識別している', alpha_state_def_id: 15, item_level:5, item_point:4 },
+    { id: 114, dseq:'7', ddescription: 'チームはリリースのために追加作業が必要かを検討している', alpha_state_def_id: 15, item_level:5, item_point:4 },
+    { id: 115, dseq:'8', ddescription: 'チームはリリースに必要な追加作業を実施している', alpha_state_def_id: 15, item_level:5, item_point:5 },
+
 )
--- db/fixtures/15_alpha_item_team_def.rb
+++ db/fixtures/15_alpha_item_team_def.rb
@@ -2,53 +2,64 @@
 
 # チーム1/5 95
 AlphaItemDef.seed_once(:id,
-    { id: 95, dseq:'1', ddescription: 'メンバが決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
-    { id: 96, dseq:'2', ddescription: '/スクラムマスタ/が決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
-    { id: 97, dseq:'3', ddescription: '/オーナ/が決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
-    { id: 98, dseq:'4', ddescription: 'メンバの個性を理解し、互いに尊重している', alpha_state_def_id: 16, item_level:1, item_point:6 },
-    { id: 99, dseq:'5', ddescription: 'メンバの役割（記録係、チェック係、励まし係など）を決めている', alpha_state_def_id: 16, item_level:1, item_point:5 },
-    { id: 100, dseq:'6', ddescription: 'マスタはメンバのコミュニケーションを活発にするための支援をしている', alpha_state_def_id: 16, item_level:1, item_point:5 }
+    { id: 116, dseq:'1', ddescription: 'メンバが決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
+    { id: 117, dseq:'2', ddescription: 'スクラムチームの３つのロールを理解している', alpha_state_def_id: 16, item_level:1, item_point:2 }
+    { id: 118, dseq:'3', ddescription: '/スクラムマスタ/が決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
+    { id: 119, dseq:'4', ddescription: '/オーナ/が決まっている', alpha_state_def_id: 16, item_level:1, item_point:2 },
+    { id: 120, dseq:'5', ddescription: 'メンバの個性を理解し、互いに尊重している', alpha_state_def_id: 16, item_level:2, item_point:6 },
+    { id: 121, dseq:'6', ddescription: 'メンバの役割（記録係、チェック係、励まし係など）を決めている', alpha_state_def_id: 16, item_level:2, item_point:5 },
+
 )
 
 # チーム2/5 101
 AlphaItemDef.seed_once(:id,
-    { id: 101, dseq:'1', ddescription: 'メンバ全員が開発を開始している', alpha_state_def_id: 17, item_level:3, item_point:4 },
-    { id: 102, dseq:'2', ddescription: 'メンバ全員が定期的に集まる場所と時間を決めている', alpha_state_def_id: 17, item_level:2, item_point:4 },
-    { id: 103, dseq:'3', ddescription: '全員がミッションとスケジュールを理解している', alpha_state_def_id: 17, item_level:2, item_point:6 },
-    { id: 104, dseq:'4', ddescription: 'チームのコミュニケーションのためのルールを決めている', alpha_state_def_id: 17, item_level:2, item_point:6 },
-    { id: 105, dseq:'5', ddescription: '全員がコミュニケーションのルールを守っている', alpha_state_def_id: 17, item_level:2, item_point:3 },
-    { id: 106, dseq:'6', ddescription: 'マスタは全員参加する上での障害を理解し、障害を取り除くための支援している', alpha_state_def_id: 17, item_level:3, item_point:2 }
+    { id: 122, dseq:'1', ddescription: 'メンバ全員が開発を開始している', alpha_state_def_id: 17, item_level:1, item_point:4 },
+    { id: 123, dseq:'2', ddescription: '日次スクラムのために、チーム全員が定期的に集まる場所と時刻を決めている', alpha_state_def_id: 17, item_level:1, item_point:6 },
+    { id: 124, dseq:'3', ddescription: 'チームのコミュニケーションのためのツールと利用ルールを決めている', alpha_state_def_id: 17, item_level:2, item_point:3 },
+    { id: 125, dseq:'4', ddescription: 'マスタは、メンバが相互に理解を深め、メンバのコミュニケーションを活発にするための支援をしている', alpha_state_def_id: 17, item_level:2, item_point:4 },
+    { id: 126, dseq:'5', ddescription: 'マスタは全員参加する上での障害を理解し、障害を取り除くための支援している', alpha_state_def_id: 17, item_level:3, item_point:2 }
 )
 
 # チーム3/5 107
 AlphaItemDef.seed_once(:id,
-    { id: 107, dseq:'1', ddescription: '情報を共有する上で重視する項目（データの上限、更新間隔、簡便性など）を決定している', alpha_state_def_id: 18, item_level:2, item_point:4 },
-    { id: 108, dseq:'2', ddescription: 'チームが情報を共有するための方法（プラクティスやツール）を定義している', alpha_state_def_id: 18, item_level:2, item_point:4 },
-    { id: 109, dseq:'3', ddescription: 'チームはメンバの進捗情報を共有している', alpha_state_def_id: 18, item_level:3, item_point:3 },
-    { id: 110, dseq:'4', ddescription: 'チームはメンバの障害情報を共有している', alpha_state_def_id: 18, item_level:3, item_point:3 },
-    { id: 111, dseq:'5', ddescription: 'チームはメンバの次の計画を共有している', alpha_state_def_id: 18, item_level:3, item_point:6 },
-    { id: 112, dseq:'6', ddescription: '決定した共有法が実施され、メンバ全員が互いの状況を理解しているという実感が持てている', alpha_state_def_id: 18, item_level:3, item_point:3 },
-    { id: 113, dseq:'7', ddescription: 'マスタは情報共有を促進するための支援をしている', alpha_state_def_id: 18, item_level:3, item_point:6 }
+/    { id: 127, dseq:'1', ddescription: '日次スクラムを定期的に開催している', alpha_state_def_id: 18, item_level:1, item_point:5 },
+/    { id: 128, dseq:'2', ddescription: 'チーム全員が日次スクラムに参加している', alpha_state_def_id: 18, item_level:1, item_point:5 },
+/    { id: 129, dseq:'3', ddescription: 'マスタは、日次スクラムが定期的に実施されるようにチームを支援している', alpha_state_def_id: 18, item_level:1, item_point:6 },
+/    { id: 130, dseq:'4', ddescription: 'メンバの進捗情報を共有している', alpha_state_def_id: 18, item_level:2, item_point:3 },
+/    { id: 131, dseq:'5', ddescription: 'メンバの次の計画を共有している', alpha_state_def_id: 18, item_level:2, item_point:3 },
+/    { id: 132, dseq:'6', ddescription: 'メンバの障害情報を共有している', alpha_state_def_id: 18, item_level:2, item_point:6 },
+/    { id: 133, dseq:'7', ddescription: '全員がコミュニケーションのためのツールを使い、決めた利用ルールを守っている', alpha_state_def_id: 18, item_level:2, item_point:4 },
+    { id: 134, dseq:'8', ddescription: '決定した共有法が実施され、メンバ全員が互いの状況を理解しているという実感が持てている', alpha_state_def_id: 18, item_level:3, item_point:3 },
+    { id: 135, dseq:'9', ddescription: 'マスタは情報共有を促進するための支援をしている', alpha_state_def_id: 18, item_level:3, item_point:6 }
 )
 
 # チーム4/5 114
 AlphaItemDef.seed_once(:id,
-    { id: 114, dseq:'1', ddescription: '/スプリント/でリリースする/インクリメント/のスコープと優先順位をメンバ全員が理解している', alpha_state_def_id: 19, item_level:2, item_point:4 },
-    { id: 115, dseq:'2', ddescription: '動く/インクリメント/をリリースするためにチームの共有情報が活用されている', alpha_state_def_id: 19, item_level:3, item_point:4 },
-    { id: 116, dseq:'3', ddescription: '協力して実行するタスクを増やすための方法（プラクティス・ツール）を採用している', alpha_state_def_id: 19, item_level:2, item_point:4 },
-    { id: 117, dseq:'4', ddescription: 'メンバが協力して行うタスクが増えている', alpha_state_def_id: 19, item_level:3, item_point:4 },
-    { id: 118, dseq:'5', ddescription: 'メンバ全員がゴールに向かって協力している実感が持てている', alpha_state_def_id: 19, item_level:4, item_point:3 },
-    { id: 119, dseq:'6', ddescription: 'スプリントゴールを満たす動く/インクリメント/をスプリントでリリースしている', alpha_state_def_id: 19, item_level:3, item_point:4 },
-    { id: 120, dseq:'7', ddescription: 'マスタは効率的で楽しい開発となるようにチームを支援している', alpha_state_def_id: 19, item_level:3, item_point:4 }
+    { id: 136, dseq:'1', ddescription: '/インクリメント/をリリースするために全員が協力している', alpha_state_def_id: 19, item_level:1, item_point:4 },
+    { id: 137, dseq:'2', ddescription: '分担が明確になっている', alpha_state_def_id: 19, item_level:1, item_point:4 },
+    { id: 138, dseq:'3', ddescription: '開発作業を協力しておこなっている', alpha_state_def_id: 19, item_level:1, item_point:4 },
+    { id: 139, dseq:'4', ddescription: 'マスターは、作業が分担して行われるように支援している', alpha_state_def_id: 19, item_level:, item_point:6 },
+    { id: 140, dseq:'5', ddescription: 'メンバのコミュニケーションが増え、協力して開発するタスクが増えている', alpha_state_def_id: 19, item_level:2, item_point:4 },
+    { id: 141, dseq:'6', ddescription: 'チーム全員がゴールに向かって協力している実感が持てている', alpha_state_def_id: 19, item_level:2, item_point:4 },
+    { id: 142, dseq:'7', ddescription: '他メンバの障害を理解し、それをチームで解決するために行動している', alpha_state_def_id: 19, item_level:2, item_point:4 },
+    { id: 143, dseq:'8', ddescription: 'マスタは効率的で楽しい開発となるようにチームを支援している', alpha_state_def_id: 19, item_level:2, item_point:5 }
+
+    { id: 144, dseq:'9', ddescription: '動く/インクリメント/をリリースするためにチームの共有情報が活用されている', alpha_state_def_id: 19, item_level:3, item_point:4 },
+    { id: 145, dseq:'10', ddescription: 'メンバが協力して行うタスクが増えている', alpha_state_def_id: 19, item_level:3, item_point:4 },
+    { id: 146, dseq:'11', ddescription: 'メンバ全員がゴールに向かって協力している実感が持てている', alpha_state_def_id: 19, item_level:4, item_point:3 },
+    { id: 147, dseq:'12', ddescription: 'スプリントゴールを満たす動く/インクリメント/をスプリントでリリースしている', alpha_state_def_id: 19, item_level:3, item_point:4 },
+ 
 )
 
 # チーム5/5 121
 AlphaItemDef.seed_once(:id,
-    { id: 122, dseq:'1', ddescription: 'チームの開発状況をメンバ全員で振り返り、/KPT/にまとめている', alpha_state_def_id: 20, item_level:4, item_point:3 },
-    { id: 123, dseq:'2', ddescription: '/KPT/のPからＴが提案され、次のスプリントでTがKに改善されている', alpha_state_def_id: 20, item_level:4, item_point:3 },
-    { id: 124, dseq:'3', ddescription: 'チームがより効果的・効率的に働くための方法（プラクティス・ツール）を採用している', alpha_state_def_id: 20, item_level:4, item_point:3 },
-    { id: 125, dseq:'4', ddescription: 'バグの発生や仕様の変更などの障害にメンバ全員で対応できている', alpha_state_def_id: 20, item_level:4, item_point:4 },
-    { id: 126, dseq:'5', ddescription: 'チームの作業負荷が片寄らないように調整ができている', alpha_state_def_id: 20, item_level:4, item_point:3 },
-    { id: 127, dseq:'6', ddescription: 'チームは開発効率を高めるための方法（プラクティス・ツール）を自らで決定し採用している', alpha_state_def_id: 20, item_level:4, item_point:4 },
-    { id: 128, dseq:'7', ddescription: 'マスタは振り返りのプロセスを改善し、高いモチベーションを維持する支援をしている', alpha_state_def_id: 20, item_level:4, item_point:4 }
+    { id: 148, dseq:'1', ddescription: 'チームのコミュニケーションを振り返り、その問題点と解決法をまとめている', alpha_state_def_id: 20, item_level:1, item_point:5 },
+    { id: 149, dseq:'2', ddescription: 'チームのコミュニケーションのためのツールと利用ルールを振り返り、その問題点と解決法をまとめている', alpha_state_def_id: 20, item_level:2, item_point:4 },
+    { id: 150, dseq:'3', ddescription: 'チームの開発状況をメンバ全員で振り返り、/KPT/にまとめている', alpha_state_def_id: 20, item_level:4, item_point:3 },
+    { id: 151, dseq:'4', ddescription: '/KPT/のPからＴが提案され、次のスプリントでTがKに改善されている', alpha_state_def_id: 20, item_level:4, item_point:3 },
+    { id: 152, dseq:'5', ddescription: 'チームがより効果的・効率的に働くための方法（プラクティス・ツール）を採用している', alpha_state_def_id: 20, item_level:4, item_point:3 },
+    { id: 153, dseq:'6', ddescription: 'バグの発生や仕様の変更などの障害にメンバ全員で対応できている', alpha_state_def_id: 20, item_level:4, item_point:4 },
+    { id: 154, dseq:'7', ddescription: 'チームの作業負荷が片寄らないように調整ができている', alpha_state_def_id: 20, item_level:4, item_point:3 },
+    { id: 155, dseq:'8', ddescription: 'チームは開発効率を高めるための方法（プラクティス・ツール）を自らで決定し採用している', alpha_state_def_id: 20, item_level:4, item_point:4 },
+    { id: 156, dseq:'9', ddescription: 'マスタは振り返りのプロセスを改善し、高いモチベーションを維持する支援をしている', alpha_state_def_id: 20, item_level:4, item_point:4 }
 )
