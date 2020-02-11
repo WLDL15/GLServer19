4dfa5b0de46768cd0303f9394c0b712225c29ea9
yoshikawa naoki <millennium_myonn@yahoo.co.jp>
2020-02-11 10:33:52 +0900

    alpha_evidence修正

--- app/controllers/alpha_evidences_controller.rb
+++ app/controllers/alpha_evidences_controller.rb
@@ -56,7 +56,7 @@ class AlphaEvidencesController < ApplicationController
       end
     elsif @alpha_evidence.completed == true && !params[:alpha_evidence][:reviewed]
       # 根拠が完了だったときに、completed を false に変更する
-      changed = 0
+      changed = -1
     end
     @alpha_evidence.update_user = current_user.name
     success = true
